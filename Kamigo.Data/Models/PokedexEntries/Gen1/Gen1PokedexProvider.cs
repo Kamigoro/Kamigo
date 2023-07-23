a1df54cadb6697d23fb04e395826273edb71aa07
Dylan Piette <44063839+Kamigoro@users.noreply.github.com>
2023-07-23 17:05:48 +0200

    Tellement de nouvelles choses

---
+++ Kamigo.Data/Models/PokedexEntries/Gen1/Gen1PokedexProvider.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using System.Text.Json;
+
+namespace Kamigo.Data.Models.PokedexEntries.Gen1
+{
+    public class Gen1PokedexProvider
+    {
+        public async Task<List<PokedexEntry>> GetPokedexEntriesAsync()
+        {
+            var filePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Models\PokedexEntries\Gen1\pokedex-red-blue-yellow.json");
+            //Read from a json file and return a list of PokedexEntry
+            using (StreamReader r = new StreamReader(filePath))
+            {
+                var json = await r.ReadToEndAsync();
+                JsonSerializerOptions options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true,
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                };
+                List<PokedexEntry> items = JsonSerializer.Deserialize<List<PokedexEntry>>(json, options);
+                return items;
+            }
+        }
+    }
+}
---
+++ Kamigo.Data/Models/PokedexEntry.cs
@@ -0,0 +1,14 @@
+﻿namespace Kamigo.Data.Models
+{
+    public class PokedexEntry
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string[] Type { get; set; }
+        public float Height { get; set; }
+        public float Weight { get; set; }
+        public string[] Abilities { get; set; }
+        public string SpriteLocalUrl { get; set; }
+    }
+}
